870a0b06ec075a4bccc411ed8bbb93e3c69782d3
Akil M Hylton <hyltonakil@gmail.com>
2022-02-05 16:58:14 -0500

    Changed view state success input to a response of a moviemodel object

--- app/src/main/java/com/example/kotlinomdbapp/util/ViewState.kt
+++ app/src/main/java/com/example/kotlinomdbapp/util/ViewState.kt
@@ -1,8 +1,10 @@
 package com.example.kotlinomdbapp.util
 import com.example.kotlinomdbapp.model.MovieModel
+import retrofit2.Response
+
 // Different states in the application.
 sealed class ViewState {
     object Loading: ViewState()
-    data class Success(val movies: MovieModel) : ViewState()
+    data class Success(val movies: Response<MovieModel>) : ViewState()
     data class Error(val error: String) : ViewState()
 }
