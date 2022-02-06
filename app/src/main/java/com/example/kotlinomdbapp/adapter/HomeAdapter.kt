8bdc9b281de1c0f9eb1f66b93d5c01e05f7232c0
Akil M Hylton <hyltonakil@gmail.com>
2022-02-05 23:16:12 -0500

    Added support for on click of image to go to imdb website

--- app/src/main/java/com/example/kotlinomdbapp/adapter/HomeAdapter.kt
+++ app/src/main/java/com/example/kotlinomdbapp/adapter/HomeAdapter.kt
@@ -1,7 +1,11 @@
 package com.example.kotlinomdbapp.adapter
 
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.example.kotlinomdbapp.databinding.MovieCardBinding
 import com.example.kotlinomdbapp.model.MovieModel
@@ -19,23 +23,32 @@ class HomeAdapter(private val searchData: MovieModel) :
         holder.bindTitle(searchData.Search[position].Title)
         holder.bindDate(searchData.Search[position].Year)
         holder.bindPoster(searchData.Search[position].Poster)
+        holder.bindUrl(searchData.Search[position].imdbID)
     }
 
     class HomeViewHolder(private val binding: MovieCardBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
-        fun bindTitle(title: String) {
-            binding.movieTitle.text = title
+        fun bindTitle(title: String) = with(binding) {
+            movieTitle.text = title
         }
 
-        fun bindDate(date: String) {
-            binding.movieYear.text = date
+        fun bindDate(date: String) = with(binding) {
+            movieYear.text = date
         }
 
-        fun bindPoster(poster_url: String) {
-            Picasso.get().load(poster_url).into(binding.posterImage)
+        fun bindPoster(poster_url: String) = with(binding) {
+            Picasso.get().load(poster_url).into(posterImage)
         }
 
+        fun bindUrl(imdb_id: String) = with(binding) {
+             posterImage.setOnClickListener {
+                 val url = "https://www.imdb.com/title/${imdb_id}"
+                 val intent = Intent(Intent.ACTION_VIEW)
+                 intent.data = Uri.parse(url)
+                 startActivity(root.context, intent, Bundle.EMPTY)
+             }
+        }
         companion object {
             fun newInstance(parent: ViewGroup) = MovieCardBinding.inflate(
                 LayoutInflater.from(parent.context), parent, false
